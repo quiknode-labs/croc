e5c0839c8a380e2a63651838e3627a0babf5f539
qk-santi <94850169+qk-santi@users.noreply.github.com>
2023-05-24 16:19:38 +0200

    add qn version appendix

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -36,7 +36,7 @@ func Run() (err error) {
 	app := cli.NewApp()
 	app.Name = "croc"
 	if Version == "" {
-		Version = "v9.6.4-1fce28e"
+		Version = "v9.6.4-qn1"
 	}
 	app.Version = Version
 	app.Compiled = time.Now()
