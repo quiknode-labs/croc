8670f9ce23db760b4a1e79eb3b6c9e5ff283e01c
qk-santi <94850169+qk-santi@users.noreply.github.com>
2023-10-11 17:41:34 +0200

    linux global install

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -36,7 +36,7 @@ func Run() (err error) {
 	app := cli.NewApp()
 	app.Name = "croc"
 	if Version == "" {
-		Version = "v9.6.5-qn3"
+		Version = "v9.6.5-qn4"
 	}
 	app.Version = Version
 	app.Compiled = time.Now()
